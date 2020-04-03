f7cfff0211822cd746ee7293d535e25a6cef8c6e
RHE-Nila <61205842+RHE-Nila@users.noreply.github.com>
2020-04-03 10:36:23 +0600

    finish shipment and auth intigation

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -4,37 +4,71 @@ import './Shipment.css';
 import { useAuth } from '../Login/useAuth';
 
 const Shipment = () => {
-    const { register, handleSubmit, errors } = useForm();
-  const onSubmit = data => {}
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = data => { }
   const auth = useAuth();
-  
+
   return (
     <form className="shipment" onSubmit={handleSubmit(onSubmit)}>
-     
-      <input name="name" defaultValue={auth.user.name} ref={register({ required: true })} placeholder="Name"/>
+
+      <input name="name"
+        defaultValue={auth.user.name}
+        ref={register({ required: true })}
+        placeholder="Name" />
       {
-      errors.name && <span className="error">Name is required</span>
+        errors.name &&
+        <span className="error">
+          Name is required
+        </span>
       }
 
-      <input name="email" defaultValue={auth.user.email} ref={register({ required: true })} placeholder="Email"/>
+      <input name="email"
+        defaultValue={auth.user.email}
+        ref={register({ required: true })}
+        placeholder="Email" />
       {
-      errors.email && <span className="error">email is required</span>
+        errors.email &&
+        <span className="error">
+          email is required
+          </span>
       }
 
-      <input name="AddressLine1" ref={register({ required: true })} placeholder="Address Line 1"/>
-      {errors.AddressLine1 && <span className="error">Addresss is required</span>}
+      <input name="AddressLine1"
+        ref={register({ required: true })}
+        placeholder="Address Line 1" />
+      {errors.AddressLine1 &&
+        <span className="error">
+          Addresss is required
+        </span>}
 
-      <input name="AddressLime2" ref={register} placeholder="Address Line 2"/>
-    
-      <input name="city" ref={register({ required: true })} placeholder="City"/>
-      {errors.city && <span className="error">city is required</span>}
+      <input name="AddressLime2"
+        ref={register}
+        placeholder="Address Line 2" />
 
-      <input name="country" ref={register({ required: true })} placeholder="Country"/>
-      {errors.country && <span className="error">country is required</span>}
+      <input name="city"
+        ref={register({ required: true })}
+        placeholder="City" />
+      {errors.city &&
+        <span className="error">
+          city is required
+        </span>}
 
-      <input name="zipcode" ref={register({ required: true })} placeholder="zip code"/>
-      {errors.zipcode && <span className="error">zipcode is required</span>}
-      
+      <input name="country"
+        ref={register({ required: true })}
+        placeholder="Country" />
+      {errors.country &&
+        <span className="error">
+          country is required
+        </span>}
+
+      <input name="zipcode"
+        ref={register({ required: true })}
+        placeholder="zip code" />
+      {errors.zipcode &&
+        <span className="error">
+          zipcode is required
+        </span>}
+
       <input type="submit" />
     </form>
   )
